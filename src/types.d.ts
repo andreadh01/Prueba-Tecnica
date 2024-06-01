a16d1af44a57ffd3cddfdc938d15768c24f45bcf
Andrea <andreadh01@gmail.com>
2024-06-01 01:16:33 -0700

    add auth interfaces

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,17 @@
+import { Request } from 'express'
+import { Cliente } from './models/cliente'
+
 export interface CustomResponse<T> {
   success: boolean
   message: string
   status: number
   data: T | null
+}
+
+export interface ClienteLogueado extends Cliente {
+  token: string
+}
+
+interface AuthRequest extends Request {
+  user?: any
 }
